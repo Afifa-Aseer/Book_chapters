c71bc5cbfefd0fdb5bf8741a8ee821fd153a4b29
Afifa-Aseer <61260444+Afifa-Aseer@users.noreply.github.com>
2021-11-08 02:49:47 +0500

    Add files via upload

---
+++ chapters/chapters/chap2.js
@@ -0,0 +1,42 @@
+// Chapter 2
+// Q1: Declare a variable called username
+document.write("<h1> Chapter 2  </h1>");
+
+var username;
+
+// Q2:Declare a variable called myName & assign to it a string 
+// that represents your Full Name.
+var myName = "Afifa Aseer Hurmat";
+
+// Q3: 
+var message;
+message ="Hello World";
+alert(message)
+
+// Q4
+var std_name="Alayna";
+var std_age="24 year old";
+var std_enrol="Certified Mobile Application Development"
+alert(std_name)
+alert(std_age)
+alert(std_enrol)
+
+// Q5
+alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+
+// Q6
+var email="example@js.com";
+alert("My email address is "+email);
+
+
+// Q7
+var book ="A smarter way to learn JavaScript";
+alert("I am trying to learn from a Book "+book);
+
+// Q8
+document.write("Yah! I can write HTML content through JavaScript")
+
+
+// Q9
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
